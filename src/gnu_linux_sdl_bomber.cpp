c43ceb02ff94020b535f7914014c00a6d83bb9f8
Dmitry Fedotov <dm.fe@yandex.ru>
2021-08-28 15:16:54 +0300

    Allocating a Back Buffer.

--- src/gnu_linux_sdl_bomber.cpp
+++ src/gnu_linux_sdl_bomber.cpp
@@ -1,56 +1,132 @@
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 
-bool HandleEvent(SDL_Event *Event) {
-    bool ShouldQuit = false;
+#define internal static
+#define local_persist static
+#define global_variable static
 
-    switch(Event->type) {
-        case SDL_WINDOWEVENT: {
-            switch(Event->window.event) {
-                case SDL_WINDOWEVENT_RESIZED: {
-                    SDL_Log("Window Resize(%d, %d)", Event->window.data1, Event->window.data2);
-                    break;
-                }
-                case SDL_WINDOWEVENT_EXPOSED: {
-                    SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
-                    SDL_Renderer *Renderer = SDL_GetRenderer(Window);
-                    static bool IsWhite = true;
+typedef struct
+{
+    SDL_Window *Window;
+    SDL_Renderer *Renderer;
+} SDL_WinRender;
 
-                    if (IsWhite) {
-                        SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
-                        IsWhite = false;
-                    } else {
-                        SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
-                        IsWhite = true;
-                    }
+// TODO: This is a global for now
+global_variable bool Running;
+global_variable SDL_Texture *Texture;
+global_variable void *Pixels;
+global_variable int TextureWidth;
 
-                    SDL_RenderClear(Renderer);
-                    SDL_RenderPresent(Renderer);
+internal SDL_WinRender
+GetSDL_WinRender(SDL_Event *Event)
+{
+    SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
+    SDL_Renderer *Renderer = SDL_GetRenderer(Window);
 
-                    break;
-                }
-            }
-            break;
-        }
-        case SDL_QUIT: {
-            SDL_Log("Exiting Bomber Hero...");
-            ShouldQuit = true;
-            break;
-        }
-    }
-
-    return (ShouldQuit);
+    return SDL_WinRender
+    {
+        Window,
+        Renderer
+    };
 }
 
-int main(int argc, char *argv[]) {
+internal void
+SDLResizeTexture(SDL_Renderer *Renderer, int Width, int Height)
+{
+    if (Pixels)
+    {
+        free(Pixels);
+    }
+    if (Texture)
+    {
+        SDL_DestroyTexture(Texture);
+    }
+
+    Texture = SDL_CreateTexture(Renderer,
+                                SDL_PIXELFORMAT_ARGB8888,
+                                SDL_TEXTUREACCESS_STREAMING,
+                                Width,
+                                Height);
+    TextureWidth = Width;
+    Pixels = malloc(Width * Height * 4);
+}
+
+internal void
+SDLUpdateWindow(SDL_Renderer *Renderer)
+{
+    if (Texture) {
+        SDL_UpdateTexture(Texture,
+                          0,
+                          Pixels,
+                          TextureWidth * 4);
+
+        SDL_RenderCopy(Renderer,
+                       Texture,
+                       0,
+                       0);
+    }
+    else {
+        // TODO: There is no texture!
+        SDL_LogError(0, "There is no texture!");
+    }
+
+    SDL_RenderPresent(Renderer);
+}
+
+internal void
+HandleEvent(SDL_Event *Event)
+{
+    switch(Event->type)
+    {
+        case SDL_QUIT:
+        {
+            SDL_LogDebug(0, "SDL_QUIT");
+            Running = false;
+        } break;
+
+        case SDL_WINDOWEVENT:
+        {
+            switch(Event->window.event)
+            {
+                case SDL_WINDOWEVENT_SIZE_CHANGED:
+                {
+                    SDL_LogDebug(0, "SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)", Event->window.data1, Event->window.data2);
+
+                    SDL_WinRender WinRend = GetSDL_WinRender(Event);
+                    SDLResizeTexture(WinRend.Renderer, Event->window.data1, Event->window.data2);
+                } break;
+
+                case SDL_WINDOWEVENT_FOCUS_GAINED:
+                {
+                    SDL_LogDebug(0, "SDL_WINDOWEVENT_FOCUS_GAINED");
+                } break;
+
+                case SDL_WINDOWEVENT_EXPOSED:
+                {
+                    SDL_LogDebug(0, "SDL_WINDOWEVENT_EXPOSED");
+
+                    SDL_WinRender WinRend = GetSDL_WinRender(Event);
+                    SDLUpdateWindow(WinRend.Renderer);
+                } break;
+            }
+        } break;
+    }
+}
+
+int
+main(int argc, char *argv[])
+{
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
 
     SDL_Window *Window;
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
         // TODO: SDL_Init didn't work!
         SDL_LogError(0, "Unable to initialize SDL: %s", SDL_GetError());
     }
 
-    SDL_LogInfo(0, "This is Bomber Hero!");
+    SDL_LogDebug(0, "Starting Bomber Hero!");
 
     Window = SDL_CreateWindow("Bomber Hero",
                               SDL_WINDOWPOS_UNDEFINED,
@@ -59,21 +135,27 @@ int main(int argc, char *argv[]) {
                               480,
                               SDL_WINDOW_RESIZABLE);
 
-    if (Window) {
+    if (Window)
+    {
         SDL_Renderer *Renderer = SDL_CreateRenderer(Window, -1, 0);
-        if (Renderer) {
-            for (;;) {
+
+        if (Renderer)
+        {
+            Running = true;
+            while (Running) {
                 SDL_Event Event;
                 SDL_WaitEvent(&Event);
-                if (HandleEvent(&Event)) {
-                    break;
-                }
+                HandleEvent(&Event);
             }
-        } else {
-            //TODO: Unable to create Renderer!
         }
-    } else {
-        //TODO: Unable to create Window!
+        else
+        {
+            // TODO: Unable to create Renderer!
+        }
+    }
+    else
+    {
+        // TODO: Unable to create Window!
     }
 
     SDL_Quit();
